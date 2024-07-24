136640aea7a8c537c84fda94e5c635b9a8cff2be
Phi Long <philongnguyen1704@gmail.com>
2024-07-24 22:17:03 +0700

    yey

---
+++ C_Corona.cpp
@@ -0,0 +1,50 @@
+/*
+    Code by: KoKoDuDu
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define db double
+#define pll pair<int, int>
+#define pdb pair<db, db>
+#define vector2d(name, sz1, sz2, type, val) vector<vector<type>> name(sz1, vector<type>(sz2, val))
+#define vector3d(name, sz1, sz2, sz3, type, val) vector<vector<vector<type>>> name(sz1, vector<vector<type>>(sz2, vector<type>(sz3, val)))
+#define vector4d(name, sz1, sz2, sz3, sz4, type, val) vector<vector<vector<vector<type>>>> name(sz1, vector<vector<vector<type>>>(sz2, vector<vector<type>>(sz3, vector<type>(sz4, val))))
+
+const int MOD = 1e9 + 7;
+const int INF = 1e18;
+void solve()
+{
+    string s;
+    while (cin >> s)
+    {
+        int n = s.size();
+        vector2d(dp, n + 1, n + 1, int, 0);
+        vector<vector<int>> index(30, vector<int>());
+        s = " " + s;
+        int ans = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j : index[s[i] - 'a'])
+            {
+                dp[j][i] = dp[j - 1][i - 1] + 1;
+                ans += dp[j][i] * (dp[j][i] + 1) / 2;
+            }
+            index[s[i] - 'a'].push_back(i);
+        }
+        cout << ans << "\n";
+    }
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int numTest = 1;
+    while (numTest--)
+    {
+        solve();
+    }
+    return 0;
+}
